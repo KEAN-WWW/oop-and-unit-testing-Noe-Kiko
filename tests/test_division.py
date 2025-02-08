9982e5d92f549d636e4891ca6e6a10da2b221318
Noe Condado-Lino <noecondado@Noes-MacBook-Pro.local>
2025-02-07 19:26:55 -0500

    second commit

--- tests/test_division.py
+++ tests/test_division.py
@@ -1,7 +1,11 @@
 import pytest
+from app.division import divide
+
 def test_division():
-    pass
+    """ Divide the numbers"""
+    assert divide(4, 4) == 1
 
 def test_divide_zero_exception():
+    """Divide by zero"""
     with pytest.raises(ZeroDivisionError):
-        pass
+        divide(2,0)
